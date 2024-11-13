8484461f9aaf312cf2d167dfbaa75c1d83e5a71c
Ari <164577995+skibidischool@users.noreply.github.com>
2024-11-13 02:05:14 +0000

    idk did stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,11 @@
 <script lang="ts">
-    
+    import Task from "./lib/Task.svelte";
+    let tasks = [];
+
+    function newTask() {
+        tasks.push("dude")
+    }
 </script>
+
+<button onclick={newTask}>New Task</button>
+<input id="newtask">
--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -1,3 +1,19 @@
 <script lang="ts">
-    
+    let { name } = $props();
+    let checked: boolean = $state(false);
+
+    function completeTask() {
+        checked = !checked;
+    }
 </script>
+
+<p>
+    {name}
+    <button onclick={completeTask}>
+        {#if checked}
+            Done
+        {:else}
+            Not Done
+        {/if}
+    </button>
+</p>
